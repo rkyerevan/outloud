314170c6e34e049bd4879da15489a2cde228f9ab
rkyerevan <rkyerevan@gmail.com>
2021-04-06 19:23:58 -0700

    added header and a footer component

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.scss";
+import Footer from "../components/Footer/Footer";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-     Outloud
+      <h1>Out Loud</h1>
       <span>Speaker rentals for any event</span>
+      <Footer />
     </div>
-  )
+  );
 }
