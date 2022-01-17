82e17b3d4579c2a0a21a923c50932b6836bf36f5
rkyerevan <rkyerevan@gmail.com>
2022-01-17 13:11:45 -0800

    Initial Commit

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,8 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
-     Outloud
+      Outloud
+      : Test Version
     </div>
   )
 }
